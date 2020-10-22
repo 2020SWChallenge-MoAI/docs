45d5bab95db9dd7a577e892fd229c8fc338a5ce8
Heekang Park <park.heekang33@gmail.com>
2020-10-22 17:15:55 +0900

    api doc update, 2020-10-22 17:15

--- locales/pl.js
+++ locales/pl.js
@@ -1,6 +1,6 @@
 define({
     pl: {
-        'Allowed values:'             : 'Dozwlone wartości:',
+        'Allowed values:'             : 'Dozwolone wartości:',
         'Compare all with predecessor': 'Porównaj z poprzednimi wersjami',
         'compare changes to:'         : 'porównaj zmiany do:',
         'compared to'                 : 'porównaj do:',
@@ -13,7 +13,7 @@ define({
         'No response values.'         : 'Brak odpowiedzi.',
         'optional'                    : 'opcjonalny',
         'Parameter'                   : 'Parametr',
-        'Permission:'                 : 'Uprawnienia:',
+        'Permissions:'                 : 'Uprawnienia:',
         'Response'                    : 'Odpowiedź',
         'Send'                        : 'Wyślij',
         'Send a Sample Request'       : 'Wyślij przykładowe żądanie',

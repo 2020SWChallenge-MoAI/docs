30086f0b3ed1a189be1983aa0cb592b310550354
Heekang Park <park.heekang33@gmail.com>
2020-10-24 17:41:22 +0900

    api doc update, 2020-10-24 17:41

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-24T08:26:11.145Z",
+    "time": "2020-10-24T08:41:22.670Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
